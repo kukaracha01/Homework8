7753e3f8c02c09779c0ba09d418a32f4be381c6b
mu <semkadarov@gmail.com>
2024-04-15 23:48:04 +0300

    Задание 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Task1");
+        int[] arrOne = new int[3];
+        arrOne[0] = 1;
+        arrOne[1] = 2;
+        arrOne[2] = 3;
+        System.out.print(arrOne[0] + " " + arrOne[1] + " " + arrOne[2]);
+        System.out.println();
+        double[] arrTwo = {1.57, 7.654, 9.986};
+        System.out.print(arrTwo[0] + " " + arrTwo[1] + " " + arrTwo[2]);
+        System.out.println();
+        long[] arrThree = {12L, 4L, 5};
+        System.out.print(arrThree[0] + " " + arrThree[1] + " " + arrThree[2]);
+        System.out.println();
+
+        System.out.println("Task2");
     }
 }
